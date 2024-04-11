8aa6ef7211802810f8a7da0ef3072e18d1d21c40
Evlusha <106895767+Evlusha@users.noreply.github.com>
2024-04-12 03:10:54 +0800

    Update zd2.java

--- zd2.java
+++ zd2.java
@@ -1,23 +1,60 @@
+import java.util.*;
 
-import java.util.HashSet;
-import java.util.Arrays;
+public class Main {
+    static List<String> morseAlphabet = Arrays.asList(
+            ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---",
+            ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..");
 
-public class zd2 {
-    public static void main(String[] args) {
-        String[] text = { "qaq", "qaq", "3", "2" };
-        int uniqueTextCount = Repac(text, 4);
-        System.out.println("Количество уникальных слов на азбуке Морзе: " + uniqueTextCount);
+    static Map<Character, String> charToMorse = new HashMap<>();
+
+    public static void initCharToMorseMap() {
+        for (char c = 'a'; c <= 'z'; ++c) {
+            charToMorse.put(c, morseAlphabet.get(c - 'a'));
+        }
     }
 
-    public static int Repac(String[] text, int size) {
-        HashSet<String> uniqueText = new HashSet<>();
-        for (int i = 0; i < size; i++) {
-            String word = text[i];
-            char[] wordChars = word.toCharArray();
-            Arrays.sort(wordChars);
-            String sortedWord = new String(wordChars);
-            uniqueText.add(sortedWord);
+    public static void generatePermutations(String str, int l, int r, Set<String> permutations) {
+        if (l == r) {
+            permutations.add(str);
+        } else {
+            for (int i = l; i <= r; i++) {
+                str = swapChars(str, l, i);
+                generatePermutations(str, l + 1, r, permutations);
+                str = swapChars(str, l, i);
+            }
         }
-        return uniqueText.size();
+    }
+
+    public static String swapChars(String str, int i, int j) {
+        char[] charArray = str.toCharArray();
+        char temp = charArray[i];
+        charArray[i] = charArray[j];
+        charArray[j] = temp;
+        return new String(charArray);
+    }
+
+    public static String stringToMorse(String str) {
+        StringBuilder morseString = new StringBuilder();
+        for (char c : str.toCharArray()) {
+            morseString.append(charToMorse.get(c));
+        }
+        return morseString.toString();
+    }
+
+    public static void main(String[] args) {
+        initCharToMorseMap();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите буквы для перестановки: ");
+        String input = scanner.nextLine();
+        Set<String> permutations = new HashSet<>();
+        generatePermutations(input, 0, input.length() - 1, permutations);
+        Set<String> uniqueMorseWords = new HashSet<>();
+        for (String word : permutations) {
+            uniqueMorseWords.add(stringToMorse(word));
+        }
+        System.out.print("Уникальные слова в языке Морзе: ");
+        for (String morseWord : uniqueMorseWords) {
+            System.out.print(morseWord + "|| ");
+        }
     }
 }
