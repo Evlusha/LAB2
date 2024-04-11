10d363c73e7a60cfc2cd6ef3cf9d9c6bb3c0dc2e
Evlusha <106895767+Evlusha@users.noreply.github.com>
2024-04-12 03:13:03 +0800

    Update zd2.js

--- zd2.js
+++ zd2.js
@@ -1,19 +1,51 @@
-class zd2 {
-  static Main2() {
-    const text = ["qaq", "._.", "3", "2"];
-    const uniqueTextCount = zd2.Repac(text, 4);
-    console.log("Количество уникальных слов на азбуке Морзе: " + uniqueTextCount);
-  }
+let morseAlphabet = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."];
 
-  static Repac(text, size) {
-    const uniqueText = new Set();
-    for (let i = 0; i < size; i++) {
-      let word = text[i];
-      word = word.split('').sort().join('');
-      uniqueText.add(word);
-    }
-    return uniqueText.size;
+let charToMorse = new Map();
+
+function initCharToMorseMap() {
+  for (let c = 'a'; c <= 'z'; ++c) {
+    charToMorse.set(c, morseAlphabet[c.charCodeAt(0) - 'a'.charCodeAt(0)]);
   }
 }
 
-zd2.Main2();
+function generatePermutations(str, l, r, permutations) {
+  if (l == r) {
+    permutations.add(str);
+  } else {
+    for (let i = l; i <= r; i++) {
+      str = swapChars(str, l, i);
+      generatePermutations(str, l + 1, r, permutations);
+      str = swapChars(str, l, i);
+    }
+  }
+}
+
+function swapChars(str, i, j) {
+  let arr = str.split('');
+  [arr[i], arr[j]] = [arr[j], arr[i]];
+  return arr.join('');
+}
+
+function stringToMorse(str) {
+  let morseString = "";
+  for (let c of str) {
+    morseString += charToMorse.get(c);
+  }
+  return morseString;
+}
+
+initCharToMorseMap();
+
+let input = prompt("Введите буквы для перестановки: ");
+let permutations = new Set();
+generatePermutations(input, 0, input.length - 1, permutations);
+
+let uniqueMorseWords = new Set();
+for (let word of permutations) {
+  uniqueMorseWords.add(stringToMorse(word));
+}
+
+console.log("Уникальные слова в языке Морзе: ");
+for (let morseWord of uniqueMorseWords) {
+  console.log(morseWord + "|| ");
+}
