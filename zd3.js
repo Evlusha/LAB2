2c04e04061122e168507cc366c68340743400a46
Evlusha <106895767+Evlusha@users.noreply.github.com>
2024-04-12 11:15:35 +0700

    Update zd3.js

--- zd3.js
+++ zd3.js
@@ -1,44 +1,41 @@
 function isPrime(n) {
-    if (n <= 1) {
-        return false;
+  if (n < 1) {
+    return false;
+  }
+  for (let i = 2; i * i <= n; ++i) {
+    if (n % i === 0) {
+      return false;
     }
-    for (let i = 2; i * i <= n; ++i) {
-        if (n % i === 0) {
-            return false;
-        }
-    }
-    return true;
+  }
+  return true;
 }
 
 function main() {
-    const numbers = [];
-    let num;
-    // Ввод чисел
-    console.log("Введите числа (для завершения введите отрицательное число):");
-    num = parseInt(prompt("Введите число:"));
-    
-    while (true) {
-        if (num > 0) {
-            num = parseInt(prompt("Введите число:"));
-            if (num < 0) {
-                break;
-            }
-            numbers.push(num);
-        } else {
-            console.log("Нужно вводить только числа:");
-            return;
-        }
+  const numbers = [];
+  let num;
+  let sh;
+  console.log("Введите количество цифр");
+  sh = parseInt(prompt());
+  
+  console.log("Введите числа :");
+  for (let i = 0; i < sh; i++) {
+    num = parseInt(prompt());
+    if (num > 0) {
+      numbers.push(num);
+    } else {
+      console.log("Нужно вводить только числа:");
+      return;
     }
-    
-    // Проверка чисел на простоту
-    console.log("Результаты проверки чисел на простоту:");
-    for (let number of numbers) {
-        if (isPrime(number)) {
-            console.log(number + " - простое число");
-        } else {
-            console.log(number + " - не является простым числом");
-        }
+  }
+  
+  console.log("Результаты проверки чисел на простоту:");
+  for (let number of numbers) {
+    if (isPrime(number)) {
+      console.log(number + " - простое число");
+    } else {
+      console.log(number + " - не является простым числом");
     }
+  }
 }
 
 main();
