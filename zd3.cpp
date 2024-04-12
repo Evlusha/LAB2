ac6d924cc080a2c7a444ecf80fb1f8fd2e4250a8
Evlusha <106895767+Evlusha@users.noreply.github.com>
2024-04-12 10:51:36 +0700

    Update zd3.cpp

--- zd3.cpp
+++ zd3.cpp
@@ -3,63 +3,61 @@
 #include <vector>
 using namespace std;
 
-    bool is_prime(int n)
+bool is_prime(int n)
+{
+    if (n < 1)
     {
-        if (n <= 1)
+        return false;
+    }
+    for (int i = 2; i * i <= n; ++i)
+    {
+        if (n % i == 0)
         {
             return false;
         }
-        for (int i = 2; i * i <= n; ++i)
-        {
-            if (n % i == 0)
-            {
-                return false;
-            }
-        }
-        return true;
     }
+    return true;
+}
 
-    int main() {
-        setlocale(LC_ALL, "Russian");
-        vector<int> numbers;
-        int num;
-
-        // Ввод чисел
-        cout << "Введите числа (для завершения введите отрицательное число):\n";
-       cin >> num;  
-         
-        while (true)
+int main() {
+    setlocale(LC_ALL, "Russian");
+    vector<int> numbers;
+    int num;
+    int sh;
+    cout << "Введите количество цифр\n";
+    cin >> sh;
+    // Ввод чисел
+    cout << "Введите числа :\n";
+   
+        for (int i = 0; i < sh; i++)
         {
+            cin >> num;
             if (num > 0)
             {
-                cin >> num;
-                if (num < 0)
-                {
-                   break;
-                }
                 numbers.push_back(num);
-             }
-            else 
-            {
-               cout << "Нужно вводить только числа:\n";
-
-               return 0;
-            }
-        }      
-
-        // Проверка чисел на простоту
-        cout << "Результаты проверки чисел на простоту:\n";
-        for (int number : numbers)
-        {
-            if (is_prime(number))
-            {
-                cout << number << " - простое число\n";
             }
             else
             {
-                cout << number << " - не является простым числом\n";
+                cout << "Нужно вводить только числа:\n";
+
+                return 0;
             }
         }
+    
 
-       
-    } 
+    // Проверка чисел на простоту
+    cout << "Результаты проверки чисел на простоту:\n";
+    for (int number : numbers)
+    {
+        if (is_prime(number))
+        {
+            cout << number << " - простое число\n";
+        }
+        else
+        {
+            cout << number << " - не является простым числом\n";
+        }
+    }
+
+
+}
