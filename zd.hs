698a13997d72ab17888cc314fcb91b7b4c976b8b
Evlusha <106895767+Evlusha@users.noreply.github.com>
2024-04-15 05:57:27 +0800

    Update zd.hs

--- zd.hs
+++ zd.hs
@@ -1,23 +1,24 @@
 isPrime :: Int -> Bool
 isPrime n
-  | n <= 1 = False
+  | n < 1 = False
   | otherwise = all (\i -> n `mod` i /= 0) [2..isqrt n]
   where isqrt = floor . sqrt . fromIntegral
 
 main :: IO ()
 main = do
-  putStrLn "Введите числа (для завершения введите отрицательное число):"
-  numbers <- getNumbers []
+  putStrLn "Введите количество цифр"
+  sh <- readLn
+  numbers <- getNumbers sh []
   putStrLn "Результаты проверки чисел на простоту:"
   mapM_ (\number -> if isPrime number then putStrLn (show number ++ " - простое число") else putStrLn (show number ++ " - не является простым числом")) numbers
 
-getNumbers :: [Int] -> IO [Int]
-getNumbers acc = do
-  num <- readLn :: IO Int
-  if num < 0
-    then return (reverse acc)
-    else if num > 0
-      then getNumbers (num : acc)
-      else do
-        putStrLn "Нужно вводить только числа:"
-        return []
+getNumbers :: Int -> [Int] -> IO [Int]
+getNumbers 0 acc = return (reverse acc)
+getNumbers sh acc = do
+  putStrLn "Введите число:"
+  num <- readLn
+  if num > 0
+    then getNumbers (sh - 1) (num : acc)
+    else do
+      putStrLn "Нужно вводить только числа:"
+      return []
